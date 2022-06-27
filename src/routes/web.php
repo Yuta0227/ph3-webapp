68cc446cf2913bf0958b60ab26a14f33851e7c0f
Yuta0227 <yutahonjo@keio.jp>
2022-06-27 19:45:50 +0900

    環境構築できた。ここからブランチ切っていく

--- src/routes/web.php
+++ src/routes/web.php
@@ -11,6 +11,13 @@
 |
 */
 
+use Illuminate\Support\Facades\Route;
+
 Route::get('/', function () {
     return view('welcome');
 });
+// Route::get('/webapp','WebappController@index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
