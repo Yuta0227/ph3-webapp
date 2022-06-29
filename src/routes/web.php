11251221e5dc9866b195988ddc6b1157584c17b7
Yuta0227 <yutahonjo@keio.jp>
2022-06-29 16:00:59 +0900

    webapp最低限完成

--- src/routes/web.php
+++ src/routes/web.php
@@ -27,7 +27,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/webapp','WebappController@index')->name('webapp');
-Route::get('/logout','WebappController@logout');
+Route::post('/logout','WebappController@logout')->name('logout');
 Route::post('/month','WebappController@month');
 Route::post('/send_mail',function(Request $request){
     $user=User::where('email',$request->to)->get();
